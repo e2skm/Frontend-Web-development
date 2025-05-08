a34948c724d454b0a68d199cc8cd310d003ff70f
MR. I SEKOMA <sekomaitumeleng@gmail.com>
2025-05-08 04:52:15 +0200

    Add files via upload

---
+++ javascript-amazon-project/scripts/amazon.js
@@ -0,0 +1,92 @@
+// Import variables from  module
+import {cart} from '../data/cart.js';
+import {products} from '../data/products.js';
+// Generate the html for each product
+// Variable to store generated html
+let productsHTML = '';
+// Function to generate the html and update variable
+products.forEach( (product) => {
+  productsHTML += `
+    <div class="product-container">
+      <div class="product-image-container">
+        <img class="product-image"
+          src="${product.image}">
+      </div>
+
+      <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
+
+      <div class="product-rating-container">
+        <img class="product-rating-stars"
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
+        </div>
+      </div>
+
+      <div class="product-price">
+        $${(product.priceInCents / 100).toFixed(2)}
+      </div>
+
+      <div class="product-quantity-container">
+        <select>
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+
+      <div class="product-spacer"></div>
+
+      <div class="added-to-cart">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
+
+      <button class="add-to-cart-button button-primary js-add-to-cart" 
+      data-product-id="${product.id}">
+        Add to Cart
+      </button>
+    </div>
+  `;
+});
+// Place generated html in the html file using dom
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+// Interactivity 
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if(productId === item.productId){
+        matchingItem = item;
+      }
+    });
+    if (matchingItem) matchingItem.quantity ++;
+    else{
+      cart.push({
+        productId,
+        quantity: 1
+      });
+    }
+
+    // Variable to store the number of items in our cart
+    let cartQuantity = 0;
+    // Function to update the quantity of items in our cart
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+    // Display cart quantity on our web page
+    document.querySelector('.js-cart-quantity').innerText = cartQuantity;
+  });
+});
+
