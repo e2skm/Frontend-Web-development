2360b9866627386e6b9bf11ca9d8d0e985b0d147
MR. I SEKOMA <sekomaitumeleng@gmail.com>
2025-05-12 18:14:48 +0200

    Add files via upload

---
+++ javascript-amazon-project/scripts/amazon.js
@@ -0,0 +1,95 @@
+// Import variables from  module
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
+// Generate the html for each product
+// Variable to store generated html
+let productsHTML = '';
+// Function to generate the html and update variable
+products.forEach( (product) => {
+  productsHTML += `
+    <div class="product-container">
+      <div class="product-image-container">
+        <img class="product-image"
+          src="${product.image}">
+      </div>
+
+      <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
+
+      <div class="product-rating-container">
+        <img class="product-rating-stars"
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
+        </div>
+      </div>
+
+      <div class="product-price">
+        $${formatCurrency(product.priceInCents)}
+      </div>
+
+      <div class="product-quantity-container">
+        <select class="js-quantity-selector-${product.id}">
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+
+      <div class="product-spacer"></div>
+
+      <div class="added-to-cart js-added-to-cart-${product.id}">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
+
+      <button class="add-to-cart-button button-primary js-add-to-cart" 
+      data-product-id="${product.id}">
+        Add to Cart
+      </button>
+    </div>
+  `;
+});
+// Place generated html in the html file using dom
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+// Function to update cart quantity
+function updateCartQuantity(){
+  // Variable to store the number of items in our cart
+  let cartQuantity = 0; 
+  // Function to update the quantity of items in our cart
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+  // Display cart quantity on our web page
+  document.querySelector('.js-cart-quantity').innerText = JSON.parse(localStorage.getItem('cartQuantity'));
+
+  // Save to localStorage
+  localStorage.setItem('cartQuantity', cartQuantity);
+}
+// Make add to cart buttons interactive 
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+    const quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+    console.log(quantity);
+    addToCart(productId,quantity);
+    updateCartQuantity();
+    // Diplay pop up meassage to show item is added to cart
+    const addedToCartElement = document.querySelector(`.js-added-to-cart-${productId}`);
+    setTimeout(() => {
+      addedToCartElement.classList.remove('added-to-cart-message');      
+    }, 2000); 
+    addedToCartElement.classList.add('added-to-cart-message');
+  });
+});
+// Display cart quantity on the web page
+updateCartQuantity();
